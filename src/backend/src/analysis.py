0a04d2b6d3271cdb109c83c7fd6cd9faf7c62756
Nour125 <ahmed.noureddin.00@gmail.com>
2024-10-24 14:14:22 +0200

    Refactor and enhance file handling

--- src/backend/src/analysis.py
+++ src/backend/src/analysis.py
@@ -2,7 +2,13 @@ import os
 import pandas as pd
 from qel_simulation import *
 from qrpm.analysis.quantityState import determine_quantity_state_qel
-from qrpm.analysis.dataImport import load_qel_from_file
+from qrpm.analysis.dataImport import load_qel_from_file, ImporterQEL
+from qrpm.analysis.modelDiscovery import mine_basic_qnet_from_qel
+from qel_simulation.components.quantity_net_graph import (
+    QuantityGraph,
+    show_qnet_graph,
+    export_qnet_graph,
+)
 
 
 # get the last uploaded file from the files directory
@@ -13,10 +19,16 @@ def get_last_uploaded_file():
     return files[-1]
 
 
-####### THIS IS NOT WORKING #######
-t = get_last_uploaded_file()
-qel = load_qel_from_file(t)
+# get the last uploaded file + the path
+t = "src//backend//files//" + get_last_uploaded_file()
+
+# load the qel from the file
+qel = load_qel_from_file(file_path=t)
 
 # get the quantity state from the qel
 qs = determine_quantity_state_qel(qel)
 print(qs)
+
+# Discovery of the QNet
+qnet = mine_basic_qnet_from_qel(qel)
+s = show_qnet_graph(qnet)
